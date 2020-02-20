3d5159faa9fb5d9a07705d89eabb65206d0de6db
Luong NGUYEN <luongnv89@gmail.com>
2020-02-20 13:46:20 +0100

    Status of THING

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const ONLINE = 'ONLINE';
+const OFFLINE = 'OFFLINE';
+const SIMULATING = 'SIMULATING';
+const PAUSE = 'PAUSE';
+const STOP = 'STOP';
+
+module.exports = {
+  ONLINE,
+  OFFLINE,
+  SIMULATING,
+  PAUSE,
+  STOP
+}
