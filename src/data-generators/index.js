0fe47b89f489d701b8ea2f5b4f331d1d96b18d4b
Luong NGUYEN <luongnv89@gmail.com>
2020-02-20 16:32:31 +0100

    Refactory data-generators with the configuration file possible to generate data for multiples sensors and actuators at one execution

--- src/data-generators/index.js
+++ src/data-generators/index.js
@@ -1,94 +1,64 @@
-const { ENACTDB, Actuator, Sensor } = require("../enact-mongoose");
+const { ENACTDB, ActuatorSchema, SensorSchema } = require("../enact-mongoose");
 const DataGenerator = require("./DataGenerator");
-const dbConfig = require("../config").MONGODB;
+const { readJSONFile } = require('../utils');
 
-const showHelp = () => {
-  console.log(`Usage:
-    node index.js <dev-type> <dev-id> <data-source>
-  Options:
-    dev-type    Device type. Can be "sensor" or "actuator"
-    dev-id      Device id
-    data-sources    the source of the data:
-      random <data-define> <time-interval> [<start-time> [duration]]: randomly generates regular data based on the data-define
-        data-define: a json file to defined the type of data
-        time-interval: time period to generate data
-        start-time: time to start. Current time if NULL
-        duration: duration time to generate data -> infinitite if NULL
-  `);
-};
+const SENSOR_TYPE = 'sensor';
+const ACTUATOR_TYPE = 'actuator';
 
-const invalidArgument = errorMessage => {
-  console.error(`[ERROR] cannot start data generator. ${errorMessage}`);
-  showHelp();
-  process.exit(1);
-};
 
-// Parse the command-line parameters
-const devType = process.argv[2];
-if (devType === "help") {
-  showHelp();
-  process.exit(0);
-} else if (devType !== "sensor" && devType !== "actuator") {
-  invalidArgument("Invalid dev-type value!");
-}
+let enactDB = null; // Enact MongoDB Client
+const dataConfigFile = process.argv[2]; // the path to data configuration file
 
-if (process.argv.length < 5) {
-  invalidArgument("Missing argument!");
-}
-
-// Clean and exit the simulation
-const cleanAndExit = () => {
-  if (enactDB) {
-    setTimeout(() => {
-      enactDB.close();
-    }, 3000);
-  }
-};
-
-// Parse first arguments
-const devID = process.argv[3];
-
-let enactDB = null;
-if (dbConfig.USER && dbConfig.PASSWORD) {
-  enactDB = new ENACTDB(dbConfig.HOST, dbConfig.PORT, dbConfig.DBNAME, {
-    userName: dbConfig.USER,
-    password: dbConfig.PASSWORD
-  });
-} else {
-  enactDB = new ENACTDB(dbConfig.HOST, dbConfig.PORT, dbConfig.DBNAME);
-}
-
-const saveData = (data, generatedTime) => {
-  if (devType == "actuator") {
-    const newActuator = new Actuator({
+/**
+ * Save a generated data into database
+ * @param {String} type The type of device: sensor or actuator
+ * @param {String} id The id of the device
+ * @param {Object} data The data to be stored
+ * @param {Number} generatedTime The generated time of the data
+ */
+const saveData = (type, id, data, generatedTime) => {
+  if (type == ACTUATOR_TYPE) {
+    const newActuator = new ActuatorSchema({
       timestamp: generatedTime,
-      actID: devID,
+      actID: id,
       value: JSON.stringify(data)
     });
-    newActuator.save();
-  } else if (devType == "sensor") {
-    const newSensor = new Sensor({
+    newActuator.save((err, _data) => {
+      if (err) {
+        console.error(`[ERROR] Failed to save generated data of actuator ${id}`);
+      } else {
+        console.log(`[Actuator ${id}] ${generatedTime} ${JSON.stringify(data)}`);
+      }
+    });
+  } else if (type == SENSOR_TYPE) {
+    const newSensor = new SensorSchema({
       timestamp: generatedTime,
-      sensorID: devID,
+      sensorID: id,
       value: JSON.stringify(data)
     });
-    newSensor.save();
+    newSensor.save((err, _data) => {
+      if (err) {
+        console.error(`[ERROR] Failed to save generated data of sensor ${id}`);
+      } else {
+        console.log(`[Sensor ${id}] ${generatedTime} ${JSON.stringify(data)}`);
+      }
+    });
   }
 };
 
-// Generate data randomly
-const generateRandomData = () => {
-  if (process.argv.length < 7) {
-    cleanAndExit();
-    invalidArgument("Missing argument!");
-  }
-  const dataDescription = JSON.parse(process.argv[5]);
-  const timeInterval = Number(process.argv[6]);
-
+/**
+ * Start generating data for a device
+ * @param {String} type The type of the device
+ * @param {String} id The id of the device
+ * @param {Number} timeInterval The generating time interval - in seconds
+ * @param {Number} _startTime The generating started time - in timestamp format
+ * @param {Number} duration The duration of the generating - in seconds
+ * @param {Object} dataDescription The definition of the genereated data
+ */
+const startGeneratingData = (type, id, timeInterval, _startTime, duration, dataDescription) => {
   const dataGenerator = new DataGenerator(dataDescription, timeInterval);
-  let startTime = Number(process.argv[7]);
   let timeDelta = 0;
-  const timePeriod = Number(process.argv[8]);
+  let startTime = _startTime;
   if (!startTime) {
     startTime = Date.now();
   } else {
@@ -98,39 +68,57 @@ const generateRandomData = () => {
     `Generate randomly data ${dataDescription.type} and publish the data in every ${timeInterval} seconds`
   );
   console.log(
-    `Start Time ${startTime} and duration time ${timePeriod} seconds`
+    `Start Time ${startTime} and duration time ${duration} seconds`
   );
   let generatorID = setInterval(() => {
     dataGenerator.generateData(data => {
       const generatedTime = Date.now() - timeDelta;
-      saveData(data, generatedTime);
-      if (timePeriod && generatedTime - startTime > timePeriod * 1000) {
-        console.log("Finish!");
+      saveData(type, id, data, generatedTime);
+      if (duration && generatedTime - startTime > duration * 1000) {
+        console.log(`[${type} ${id}] Stopped generating data!`);
         clearInterval(generatorID);
-        cleanAndExit();
       }
     });
   }, timeInterval * 1000);
 };
 
-// Handle data sources
-const startGeneratingData = dataSources => {
-  switch (dataSources) {
-    case "random":
-      generateRandomData();
-      break;
-    default:
-      console.error("[ERROR] Invalid data source : ", dataSources);
-      cleanAndExit();
-      break;
+readJSONFile(dataConfigFile, (err, dataConfig) => {
+  if (err) {
+    console.error(`[ERROR] Cannot read the data config file:`, dataConfigFile);
+    return;
   }
-};
 
-enactDB.connect(error => {
-  if (error) {
-    console.log("[ERROR] Failed to connect to database", error);
-    exit(1);
+  const { dbConfig, sensors, actuators } = dataConfig;
+  if (!dbConfig) {
+    console.error(`[ERROR] Missing database configuration:`, dataConfig);
+    return;
   }
-  console.log("Connected to database");
-  startGeneratingData(process.argv[4]);
+
+  if (dbConfig.USER && dbConfig.PASSWORD) {
+    enactDB = new ENACTDB(dbConfig.HOST, dbConfig.PORT, dbConfig.DBNAME, {
+      userName: dbConfig.USER,
+      password: dbConfig.PASSWORD
+    });
+  } else {
+    enactDB = new ENACTDB(dbConfig.HOST, dbConfig.PORT, dbConfig.DBNAME);
+  }
+
+  enactDB.connect(error => {
+    if (error) {
+      console.log("[ERROR] Failed to connect to database", error, dbConfig);
+      exit(1);
+    }
+    console.log("Connected to database");
+    // Generate sensors data
+    for (let sIndex = 0; sIndex < sensors.length; sIndex++) {
+      const {id, timeInterval, startTime, duration, dataDescription} = sensors[sIndex];
+      startGeneratingData(SENSOR_TYPE, id, timeInterval, startTime, duration, dataDescription);
+    }
+    // Generate actuators data
+    for (let sIndex = 0; sIndex < actuators.length; sIndex++) {
+      const {id, timeInterval, startTime, duration, dataDescription} = actuators[sIndex];
+      startGeneratingData(ACTUATOR_TYPE, id, timeInterval, startTime, duration, dataDescription);
+    }
+
+  });
 });
