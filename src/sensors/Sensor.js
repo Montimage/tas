aad1f84a5330f649ec38f4d9848ed1fee518cf58
Luong NGUYEN <luongnv89@gmail.com>
2020-02-20 13:44:55 +0100

    Simulate a sensor who collect the data by generated randomly data or read from database, then publish the data

---
+++ src/sensors/Sensor.js
@@ -0,0 +1,178 @@
+const { SIMULATING, OFFLINE } = require('../constants');
+const DataGenerator = require('../data-generators/DataGenerator');
+const { ENACTDB, SensorSchema } = require("../enact-mongoose");
+/**
+ * The Sensor class presents a sensor
+ * - Collect the data: randomly or from database
+ * - Publish the data
+ */
+class Sensor {
+  /**
+   *
+   * @param {String} id The sensor's id
+   * @param {String} topic The mqtt topic to publish the data
+   * @param {Object} dataSource The data source of the sensor
+   * -
+   */
+  constructor(id, topic, dataSource) {
+    this.id = id;
+    this.topic = topic;
+    this.dataSource = dataSource;
+    this.mqttClient = null;
+    this.status = OFFLINE;
+    this.dbClient = null;
+  }
+
+  /**
+   * Publish data
+   * @param {String} strData data to be published
+   */
+  publishData(strData) {
+    // console.log("Going to send data", this.topic, strData);
+    this.mqttClient.publish(this.topic, JSON.stringify(strData));
+  }
+
+  /**
+   * Publish list of sensor's data
+   * @param {Array} listData List of sensor's data to be published
+   */
+  publishDataWithTimestamp(listData) {
+    this.status = SIMULATING;
+    const startTime = listData[0].timestamp;
+    for (let index = 0; index < listData.length; index++) {
+      if (this.status === SIMULATING) {
+        setTimeout(() => {
+          if (this.status === SIMULATING) {
+            const dataToBePublished = JSON.parse(listData[index].value);
+            // console.log(
+            //   "Going to send data: ",
+            //   index,
+            //   Date.now(),
+            //   listData[index].timestamp
+            // );
+            // console.log(dataToBePublished);
+            console.log(`sensor ${this.id}: `, dataToBePublished);
+            this.publishData(dataToBePublished);
+            if (index === listData.length - 1) {
+              this.status = OFFLINE;
+            }
+          }
+        }, listData[index].timestamp - startTime);
+      }
+    }
+  }
+
+  /**
+   * Generate randomly sensor' data
+   */
+  generateRandomData() {
+    const { dataDescription, timeInterval } = this.dataSource;
+    console.log(
+      `Generate randomly data ${dataDescription.type} and publish the data in every ${timeInterval} seconds`
+    );
+    const dataGenerator = new DataGenerator(dataDescription, timeInterval);
+    const timerID = setInterval(() => {
+      if (this.status === SIMULATING) {
+        dataGenerator.generateData(data => {
+          console.log(`sensor ${this.id}: `, data);
+          this.publishData(data);
+        });
+      } else {
+        clearInterval(timerID);
+      }
+    }, timeInterval * 1000);
+  }
+
+  /**
+   * Read data from a database and publish the data based on the timestamp, sensor id
+   *
+   */
+  readDataFromDatabase() {
+    const { startTime, dbConfig } = this.dataSource;
+    const endTime = this.dataSource.endTime
+      ? this.dataSource.endTime
+      : Date.now();
+    console.log(
+      `Read data from database ${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DBNAME}`
+    );
+    console.log(`StartTime: ${startTime}, endTime: ${endTime}`);
+    if (dbConfig.USER && dbConfig.PASSWORD) {
+      this.dbClient = new ENACTDB(dbConfig.HOST, dbConfig.PORT, dbConfig.DBNAME, {
+        userName: dbConfig.USER,
+        password: dbConfig.PASSWORD
+      });
+    } else {
+      this.dbClient = new ENACTDB(dbConfig.HOST, dbConfig.PORT, dbConfig.DBNAME);
+    }
+    this.dbClient.connect(() => {
+      console.log("Connected to database");
+      SensorSchema.findSensorDataBetweenTimes(
+        { sensorID: this.id },
+        startTime,
+        endTime,
+        (err, listData) => {
+          if (err) {
+            console.error(
+              `[ERROR] Cannot find any data for sensor ${this.id}`,
+              err
+            );
+            this.status = OFFLINE;
+          } else {
+            console.log("Number of data: ", listData.length);
+            this.dbClient.close();
+            if (listData.length > 0) {
+              this.publishDataWithTimestamp(listData);
+            } else {
+              console.error(
+                `[ERROR] Cannot find any data for sensor ${this.id}`,
+                err
+              );
+              this.status = OFFLINE;
+            }
+          }
+        }
+      );
+    });
+  }
+
+  /**
+   * Start simulating a sensor
+   * @param {Object} mqttClient The mqtt client to publish data
+   */
+  start(mqttClient) {
+    if (this.status === OFFLINE) {
+      console.log(`[Sensor ${this.id}] publishes data on channel: ${this.topic}`);
+      this.mqttClient = mqttClient;
+      if (this.dataSource.source === "random") {
+        this.status = SIMULATING;
+        this.generateRandomData();
+      } else if (this.dataSource.source === "database") {
+        this.status = SIMULATING;
+        this.readDataFromDatabase();
+      } else {
+        console.error(
+          `[ERROR] Sensor ${this.id} does not support data source: ${this.dataSource.source}`
+        );
+      }
+    } else {
+      console.log(`[SENSOR] Sensor ${this.id} is simulating!`);
+    }
+  }
+
+  /**
+   * Stop simulating the sensor
+   */
+  stop() {
+    if (this.status === OFFLINE) {
+      console.log(`[SENSOR] Sensor ${this.id} is offline!`);
+    } else {
+      this.status = OFFLINE;
+      if (this.dbClient) {
+        this.dbClient.close();
+      }
+      console.log(`[Sensor ${this.id}] stopped!`);
+    }
+  }
+}
+
+module.exports = Sensor;
