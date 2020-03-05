78a3e1d7a6a8dcce12f4aff4300bd7ddf15823b5
Luong NGUYEN <luongnv89@gmail.com>
2020-03-05 15:44:10 +0100

    Update server

--- src/dashboard/server/app.js
+++ src/dashboard/server/app.js
@@ -1,20 +1,51 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+// read and pass the environment variables into reactjs application
+const env = dotenv.config().parsed;
 
 var dataGeneratorRouter = require('./routes/data-generator');
 var simulationRouter = require('./routes/simulation');
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.set("port", env.REST_API_SERVER_PORT);
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb',  extended: true }))
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// start server
+
+var server = app.listen(app.get('port'), env.REST_API_SERVER_HOST, function () {
+   console.log('info',`Test and Simulation API Server started on: http://${env.REST_API_SERVER_HOST}:${env.REST_API_SERVER_PORT}`);
+});
+
+// Add headers
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  // res.setHeader('Access-Control-Allow-Origin', 'http://mmt.box');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, authorization');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Log the request
+  // logInfo(`${req.method} ${req.protocol}://${req.hostname}${req.path} ${res.statusCode}`);
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use('/api/data-generator', dataGeneratorRouter);
 app.use('/api/simulation', simulationRouter);
-
-module.exports = app;
--- src/dashboard/server/routes/simulation.js
+++ src/dashboard/server/routes/simulation.js
@@ -21,7 +21,7 @@ router.get('/run', function(req, res, next) {
     if (err) {
       res.send({error: 'Cannot read the configuration file'});
     } else {
-      startSimulation(thingConfigs);
+      startSimulation(thingConfigs.things);
       res.send({error: null, data: thingConfigs});
     }
   })
@@ -46,14 +46,18 @@ router.get('/', function(req, res, next) {
 
 router.post('/', (req, res, next) => {
   const newConfig = req.body.data;
-  writeToFile(configFilePath, JSON.stringify(newConfig), (err, data) => {
-    if (err) {
-      console.error(err);
-      res.send({error: 'Cannot save the new configuration'});
-    } else {
-      res.send({error: null, data: newConfig});
-    }
-  });
+  if (newConfig) {
+    writeToFile(configFilePath, JSON.stringify(newConfig), (err, data) => {
+      if (err) {
+        console.error(err);
+        res.send({error: 'Cannot save the new configuration'});
+      } else {
+        res.send({error: null, data: newConfig});
+      }
+    });
+  } else {
+    res.send({error: 'Invalid configuration!'});
+  }
 })
 
 module.exports = router;
