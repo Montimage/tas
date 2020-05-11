af7da4886590f1b4ca14a512c06c7ec7ad282bdf
Luong NGUYEN <luongnv89@gmail.com>
2020-05-11 14:54:19 +0200

    Update new build

--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.90ae3a2e4f4bd5b7fad25b856bbf8feb.js"
+  "/precache-manifest.c8e8556ec785a950903ef6829b8d43e1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
