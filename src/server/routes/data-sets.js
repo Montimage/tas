cdf2e68fe7773c691c7a84abc5c30469f5d50c13
Luong NGUYEN <luongnv@macbook-pro.home>
2020-09-30 12:02:01 +0200

    Delete events when remove dataset

--- src/server/routes/data-sets.js
+++ src/server/routes/data-sets.js
@@ -1,23 +1,21 @@
 /* Working with data set */
 const express = require("express");
+const EventSchema = require("../../core/enact-mongoose/schemas/EventSchema");
 const router = express.Router();
-const {
-  DatasetSchema,
-  dbConnector
-} = require('./db-connector');
+const { DatasetSchema, dbConnector } = require("./db-connector");
 
 router.get("/", dbConnector, function (req, res, next) {
   let page = req.query.page;
   if (!page) page = 0;
   DatasetSchema.findDatasetsWithPagingOptions(null, page, (err2, datasets) => {
     if (err2) {
-      console.error('[SERVER] Failed to get datasets', err2);
+      console.error("[SERVER] Failed to get datasets", err2);
       res.send({
-        error: 'Failed to get data set'
+        error: "Failed to get data set",
       });
     } else {
       res.send({
-        datasets
+        datasets,
       });
     }
   });
@@ -27,19 +25,17 @@ router.get("/", dbConnector, function (req, res, next) {
  * Get a data set by id
  */
 router.get("/:datasetId", dbConnector, function (req, res, next) {
-  const {
-    datasetId
-  } = req.params;
+  const { datasetId } = req.params;
 
-  DatasetSchema.findOne({id: datasetId}, (err2, dataset) => {
+  DatasetSchema.findOne({ id: datasetId }, (err2, dataset) => {
     if (err2) {
-      console.error('[SERVER] Failed to get datasets', err2);
+      console.error("[SERVER] Failed to get datasets", err2);
       res.send({
-        error: 'Failed to get data set'
+        error: "Failed to get data set",
       });
     } else {
       res.send({
-        dataset
+        dataset,
       });
     }
   });
@@ -47,16 +43,8 @@ router.get("/:datasetId", dbConnector, function (req, res, next) {
 
 // Add a new data set
 router.post("/", dbConnector, function (req, res, next) {
-  const {
-    dataset
-  } = req.body;
-  const {
-    id,
-    name,
-    tags,
-    description,
-    source,
-  } = dataset;
+  const { dataset } = req.body;
+  const { id, name, tags, description, source } = dataset;
   const currentTime = Date.now();
   const newdataset = new DatasetSchema({
     id,
@@ -65,17 +53,17 @@ router.post("/", dbConnector, function (req, res, next) {
     description,
     source,
     createdAt: currentTime,
-    lastModified: currentTime
+    lastModified: currentTime,
   });
   newdataset.save((err, _dataset) => {
     if (err) {
-      console.error('[SERVER] Failed to save the data sets', err);
+      console.error("[SERVER] Failed to save the data sets", err);
       res.send({
-        error: 'Failed to save the data set'
+        error: "Failed to save the data set",
       });
     } else {
       res.send({
-        dataset: _dataset
+        dataset: _dataset,
       });
     }
   });
@@ -85,49 +73,58 @@ router.post("/", dbConnector, function (req, res, next) {
  * Update a data set
  */
 router.post("/:datasetId", dbConnector, function (req, res, next) {
-  const {
-    dataset
-  } = req.body;
-  const {
-    datasetId
-  } = req.params;
+  const { dataset } = req.body;
+  const { datasetId } = req.params;
 
-  DatasetSchema.findOneAndUpdate({id: datasetId}, {...dataset, lastModified: Date.now()}, (err, ts) => {
-    if (err) {
-      console.error('[SERVER] Failed to save the data sets', err);
-      res.send({
-        error: 'Failed to save the data set'
-      });
-    } else {
-      res.send({
-        dataset: ts
-      });
+  DatasetSchema.findOneAndUpdate(
+    { id: datasetId },
+    { ...dataset, lastModified: Date.now() },
+    (err, ts) => {
+      if (err) {
+        console.error("[SERVER] Failed to save the data sets", err);
+        res.send({
+          error: "Failed to save the data set",
+        });
+      } else {
+        res.send({
+          dataset: ts,
+        });
+      }
     }
-  });
+  );
 });
 
 /**
  * Delete a data set by id
  */
 router.delete("/:datasetId", dbConnector, function (req, res, next) {
-  const {
-    datasetId
-  } = req.params;
+  const { datasetId } = req.params;
 
-  DatasetSchema.findOneAndDelete({id: datasetId}, (err, ret) => {
+  DatasetSchema.findOneAndDelete({ id: datasetId }, (err, ret) => {
     if (err) {
-      console.error('[SERVER] Failed to save the data sets', err);
+      console.error("[SERVER] Failed to delete the data sets");
+      console.error(err);
       res.send({
-        error: 'Failed to save the data set'
+        error: "Failed to save the data set",
       });
     } else {
-      res.send({
-        result: ret
+      EventSchema.deleteMany({ datasetId }, (err2) => {
+        if (err2) {
+          console.error(
+            "[SERVER] Failed to delete all the event of the deleted dataset"
+          );
+          console.error(err2);
+          res.send({
+            error: "Failed to delete all the event of the deleted dataset",
+          });
+        } else {
+          res.send({
+            result: ret,
+          });
+        }
       });
     }
   });
 });
-
-
 
 module.exports = router;
