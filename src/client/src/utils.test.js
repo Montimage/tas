7309ea51162b0cbb63fddfdfcbc2b2689fcecdc1
Luong NGUYEN <luongnv@macbook-pro.home>
2020-10-01 07:28:36 +0200

    update some changes

--- src/client/src/utils.test.js
+++ src/client/src/utils.test.js
@@ -1,62 +1,157 @@
-/**
- *
- * @param {Object} obj Object to be updated
- * @param {String} path Path to be updated
- * @param {Value} value the new value to be updated
- */
-const updateObjectByPath = (obj, path, value, index = null) => {
+const updateObjectByPath = (obj, path, value) => {
   let stack = path.split(".");
   while (stack.length > 1) {
-    const key = stack.shift();
-    if (!obj[key]) {
-      obj[key] = {};
+    // Not at the end of the path
+    let key = stack.shift();
+    if (key.indexOf("[") > 0) {
+      // Contains array index
+      const array = key.split("[");
+      key = array[0];
+      let index = array[1].replace("]", "");
+      if (!obj[key]) {
+        // Create a new array if it does not exist
+        obj[key] = [];
+      }
+      if (obj[key].length === 0) {
+        // Empty array
+        index = 0;
+      } else if (obj[key].length <= index || index < 0) {
+        // index out of range
+        index = obj[key].length;
+      }
+      if (!obj[key][index]) {
+        obj[key].push({});
+        // throw Error(`ERROR: Invalid data path: ${path} in object ${JSON.stringify(obj)}`);
+      }
+      obj = obj[key][index];
+    } else {
+      if (!obj[key]) {
+        // Create a new path if it does not exist
+        obj[key] = {};
+      }
+      obj = obj[key];
     }
-    obj = obj[key];
   }
   let lastKey = stack.shift();
-  if (index===null || !(obj[lastKey] instanceof Array) ) {
-    obj[lastKey] = value;
-  } else {
-    // Update an array
-    if (index < obj[lastKey].length) {
-      // Update an element in array
-      obj[lastKey][index] = value;
+  // At the end of the path
+  if (lastKey.indexOf("[") > 0) {
+    // Contains array index
+    const array = lastKey.split("[");
+    lastKey = array[0];
+    let index = array[1].replace("]", "");
+    if (value === null) {
+      // Remove an element
+      if (obj[lastKey] && obj[lastKey][index]) {
+        obj[lastKey].splice(index, 1);
+      }
     } else {
-      // add new element in array -> always append at the end of the array no matter what is the index
-      obj[lastKey].push(value);
+      // Add an element
+      if (!obj[lastKey]) {
+        // Create a new array if it does not exist
+        obj[lastKey] = [];
+      }
+      if (obj[lastKey].length === 0) {
+        // Empty array
+        index = 0;
+      } else if (obj[lastKey].length <= index || index < 0) {
+        // index out of range
+        index = obj[lastKey].length;
+      }
+      if (!obj[lastKey][index]) {
+        obj[lastKey].push(value);
+        // throw Error(`ERROR: Invalid data path: ${path} in object ${JSON.stringify(obj)}`);
+      } else {
+        obj[lastKey][index] = value;
+      }
     }
+  } else {
+    // Not contains array index
+    obj[lastKey] = value;
   }
 };
 
 const obj = {
-  id: 123,
-  name: 'Louis',
-  address: {
-    phone: "NO",
-    home:"2 residence du verger"
+  "name": "HomeIO Recorders",
+  "dataStorage": {
+    "protocol": "MONGODB",
+    "connConfig": {
+      "host": "192.168.1.21",
+      "port": 27017,
+      "username": null,
+      "password": null,
+      "dbname": "homeiodb",
+      "options": null
+    }
   },
-  accounts: [
-    {
-      name: 'BNP',
-      amount: 11
+  "dataset": {
+    "id": "homeio-dataset-02",
+    "name": "HomeIO Dataset 02",
+    "description": "This is a new dataset",
+    "tags": [
+      "recorded",
+      "conflict",
+      "homeio"
+    ]
+  },
+  "dataRecorders": [{
+      "name": "TV status recorder and Actuator",
+      "id": "homeio-recorder-01",
+      "enable": true,
+      "source": {
+        "protocol": "MQTT",
+        "connConfig": {
+          "host": "192.168.1.21",
+          "port": 1883,
+          "options": null
+        },
+        "upStreams": [
+          "enact/sensors/cec/status"
+        ],
+        "downStreams": [
+          "enact/actuators/smartbox/mute"
+        ]
+      },
+      "forward": {
+        "protocol": "MQTT",
+        "connConfig": {
+          "host": "192.168.1.21",
+          "port": 1882,
+          "options": null
+        }
+      }
     },
     {
-      name: 'LBB',
-      amount: 12
-    },
-    {
-      name: 'Bourse',
-      amount: 13
+      "name": "Call Status recorder",
+      "id": "call-status-recorder",
+      "enable": true,
+      "source": {
+        "protocol": "MQTT",
+        "connConfig": {
+          "host": "192.168.1.21",
+          "port": 1883,
+          "options": null
+        },
+        "upStreams": [
+          "call/status"
+        ],
+        "downStreams": []
+      },
+      "forward": {
+        "protocol": "MQTT",
+        "connConfig": {
+          "host": "192.168.1.21",
+          "port": 1882,
+          "options": null
+        }
+      }
     }
   ]
 };
 
-console.log(obj);
-updateObjectByPath(obj, "name","Mai");
-console.log(obj);
-updateObjectByPath(obj, "address.home","Vietnam");
-console.log(obj);
-updateObjectByPath(obj, "accounts",{name: "PP", amount: 11}, 4);
-console.log(obj);
-updateObjectByPath(obj, "accounts",{name: "PP", amount: 11}, 1);
-console.log(obj);
+console.log(JSON.stringify(obj));
+updateObjectByPath(obj, "dataStorage.connConfig.host","192.168.1.22");
+console.log('After updating...');
+console.log(JSON.stringify(obj));
+updateObjectByPath(obj, "dataRecorders[0].source.connConfig.host","192.168.1.22");
+console.log('After updating...');
+console.log(JSON.stringify(obj));
